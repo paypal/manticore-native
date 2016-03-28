83c1d0b8bd4c45f89dc8db91b1936593687ef711
Max Metral <mmetral@DM-BOS-00678031.corp.ebay.com>
2016-03-28 17:00:28 -0400

    Try and cd before xctool

--- ci/travis.sh
+++ ci/travis.sh
@@ -2,4 +2,5 @@ npm install
 npm run build-test-js
 npm run build-objc-polyfill
 npm run gen-objc-test
+cd runtime/objc
 xctool test -workspace Manticore.xcworkspace -scheme ManticoreContainer-iOS -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
