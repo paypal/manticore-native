c538ab214b45b5b59dba1d2d669307b5495a3995
Max Metral <mmetral@DM-BOS-00678031.corp.ebay.com>
2016-03-29 15:19:56 -0400

    Android fixes

--- ci/travis.sh
+++ ci/travis.sh
@@ -13,27 +13,27 @@ function osxSetup {
   npm run gen-objc-test
 }
 
-if [ "$BUILD_ITEM" == "ios" ]
+if [ "$BUILD_ITEM" == "mac" ]
 then
-  echo "=*=*=*=*=*=*=*=*=*=*=*=* BUILDING ios =*=*=*=*=*=*=*=*=*=*=*=*"
-#  osxSetup
+  echo "=*=*=*=*=*=*=*=*=*=*=*=* BUILDING mac =*=*=*=*=*=*=*=*=*=*=*=*"
+  node -v
+  npm -v
+#  brew update
+#  brew unlink node
+#  brew install homebrew/versions/node4-lts
+#  npm install -g npm@3
+#  npm install
+#  npm run build-test-js
+#  npm run build-objc-polyfill
+#  npm run gen-objc-test
 #  cd runtime/objc
 #  instruments -s devices
 #  xcodebuild test -workspace Manticore.xcworkspace -scheme ManticoreContainer-iOS -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3' | tee xcodebuild9.log | xcpretty
-elif [ "$BUILD_ITEM" == "osx" ]
-then
-  echo "=*=*=*=*=*=*=*=*=*=*=*=* BUILDING osx =*=*=*=*=*=*=*=*=*=*=*=*"
-#  osxSetup
-#  cd runtime/objc
-#  instruments -s devices
-#  xcodebuild test -workspace Manticore.xcworkspace -scheme ManticoreContainer-OSX | tee xcodebuild.log | xcpretty
 elif [ "$BUILD_ITEM" == "node" ]
 then
   echo "=*=*=*=*=*=*=*=*=*=*=*=* BUILDING node =*=*=*=*=*=*=*=*=*=*=*=*"
-#  npm -v
-#  npm install
-#  npm run lint
-#  npm test
+  npm run lint
+  npm test
 elif [ "$BUILD_ITEM" == "android" ]
 then
   echo "=*=*=*=*=*=*=*=*=*=*=*=* BUILDING android =*=*=*=*=*=*=*=*=*=*=*=*"
