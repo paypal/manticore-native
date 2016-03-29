00f0be5211edf32c92c5b9965d7bcc2069d9deaf
Max Metral <mmetral@DM-BOS-00678031.corp.ebay.com>
2016-03-29 09:23:56 -0400

    Remove ONLY_ACTIVE_ARCH on a hunch

--- ci/travis.sh
+++ ci/travis.sh
@@ -3,4 +3,4 @@ npm run build-test-js
 npm run build-objc-polyfill
 npm run gen-objc-test
 cd runtime/objc
-xctool test -workspace Manticore.xcworkspace -scheme ManticoreContainer-iOS -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
+xctool test -workspace Manticore.xcworkspace -scheme ManticoreContainer-iOS -sdk iphonesimulator
