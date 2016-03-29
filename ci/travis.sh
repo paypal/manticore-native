d2d9d999799e96ee345e09ad9ad2098d8704f331
Max Metral <mmetral@DM-BOS-00678031.corp.ebay.com>
2016-03-29 10:49:23 -0400

    Attempt multiplatform build

--- ci/travis.sh
+++ ci/travis.sh
@@ -1,8 +1,33 @@
-npm install
-npm run build-test-js
-npm run build-objc-polyfill
-npm run gen-objc-test
-cd runtime/objc
-instruments -s devices
-xcodebuild test -workspace Manticore.xcworkspace -scheme ManticoreContainer-iOS -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.2' | tee xcodebuild9.log | xcpretty
-xcodebuild test -workspace Manticore.xcworkspace -scheme ManticoreContainer-iOS -destination 'platform=iOS Simulator,name=iPhone 6,OS=8.1' | tee xcodebuild8.log | xcpretty
+#!/bin/bash
+
+function osxSetup {
+  node -v
+  npm -v
+  brew update
+  brew unlink node
+  brew install homebrew/versions/node4-lts
+  npm install -g npm@3
+  npm install
+  npm run build-test-js
+  npm run build-objc-polyfill
+  npm run gen-objc-test
+}
+
+if [ "$BUILD_ITEM" == "ios" ]
+then
+  osxSetup
+  cd runtime/objc
+  instruments -s devices
+  xcodebuild test -workspace Manticore.xcworkspace -scheme ManticoreContainer-iOS -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.2' | tee xcodebuild9.log | xcpretty
+  xcodebuild test -workspace Manticore.xcworkspace -scheme ManticoreContainer-iOS -destination 'platform=iOS Simulator,name=iPhone 6,OS=8.1' | tee xcodebuild8.log | xcpretty
+elif [ "$BUILD_ITEM" == "osx" ]
+  osxSetup
+  cd runtime/objc
+  instruments -s devices
+  xcodebuild test -workspace Manticore.xcworkspace -scheme ManticoreContainer-OSX | tee xcodebuild.log | xcpretty
+elif [ "$BUILD_ITEM" == "node" ]
+  npm install -g npm@3
+  npm install
+  npm run lint
+  npm test
+fi
