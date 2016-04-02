14bc5f09a3a6800ba0434bca8a16f1832ed98e93
Max Metral <max@pyralis.com>
2016-04-02 10:32:59 -0400

    Fix minor android syntax issues

--- runtime/common/timer.js
+++ runtime/common/timer.js
@@ -2,4 +2,4 @@ const g = global;
 const m = g.manticore;
 
 // Some VMs do not like argument count mismatch, so we fix it up here.
-g.setTimeout = g.setTimeout || ((fn,t) => m._setTimeout(fn, t||0));
+g.setTimeout = g.setTimeout || ((fn, t) => m._setTimeout(fn, t || 0));
