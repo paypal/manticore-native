85d10d2247bdc3d828bbfc1f848b17ff978cc463
Max Metral <max@pyralis.com>
2016-04-03 14:14:32 -0400

    Try to make mantify handle windows by using glob

--- mantify.js
+++ mantify.js
@@ -3,10 +3,19 @@ import fs from 'fs';
 import browserify from 'browserify';
 import path from 'path';
 import mkdirp from 'mkdirp';
+import glob from 'glob';
 
-const files = process.argv.slice(3);
+let files = process.argv.slice(3);
 const output = process.argv[2];
 const options = {};
+
+if (require('os').platform() === 'win32') {
+  let targ = [];
+  for (const file of files) {
+    targ = targ.concat(glob.sync(file));
+  }
+  files = targ;
+}
 
 mkdirp.sync(path.dirname(output));
 
