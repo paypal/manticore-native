bc31e4a6708380f33b7310d3fdb4e5cda4d28f9b
Ian Katz <iakatz@paypal.com>
2016-06-28 14:48:19 -0400

    [npm] be less aggressive on npmignores

--- scripts/packaging/make-npm-ignores.sh
+++ scripts/packaging/make-npm-ignores.sh
@@ -6,5 +6,5 @@ cd "$(git rev-parse --show-toplevel)"
 DIRS=$(find runtime -type d -depth 1 | grep -v common)
 for d in $DIRS; do
     echo "Processing '$d' for npm ignore purposes"
-    ls $d | grep -v templates > "$d/.npmignore" || echo "  Nothing to ignore"
+    ls $d | grep -v templates | grep -v "^js$" | grep -v README > "$d/.npmignore" || echo "  Nothing to ignore"
 done
